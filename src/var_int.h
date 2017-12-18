c0fac6f8cf9e01888a409c9500fd052d3c940a94
trumanzhao <16861430@qq.com>
2017-12-18 23:16:10 +0800

    Update var_int.h
    整理代码和注释

--- src/var_int.h
+++ src/var_int.h
@@ -7,12 +7,16 @@
 
 #define MAX_VARINT_SIZE  16
 
-// 将整数编码为到变长字节数组:
-// 返回值: 成功,返回变长编码长度; 失败,返回0;
+// 将无符号整数编码到字节数组
+// 返回值: 成功,返回编码长度; 失败,返回0;
 size_t encode_u64(unsigned char* buffer, size_t buffer_size, uint64_t value);
-size_t encode_s64(unsigned char* buffer, size_t buffer_size, int64_t value);
-
-// 将变长字节序列解码为整数:
-// 返回值: 成功,返回变长编码实际长度; 失败,返回0;
+// 从字节数组解码无符号整数
+// 返回值: 成功,返回解码长度; 失败,返回0;
 size_t decode_u64(uint64_t* value, const unsigned char* data, size_t data_len);
+
+// 将有符号整数编码到字节数组
+// 返回值: 成功,返回编码长度; 失败,返回0;
+size_t encode_s64(unsigned char* buffer, size_t buffer_size, int64_t value);
+// 从字节数组解码有符号整数
+// 返回值: 成功,返回解码长度; 失败,返回0;
 size_t decode_s64(int64_t* value, const unsigned char* data, size_t data_len);
